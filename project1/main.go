493c42a783d86903168146b4c201dccf085f6d17
alexshe <alexshe@wix.com>
2018-03-27 17:36:01 +0300

    etcd client

--- project1/main.go
+++ project1/main.go
@@ -3,9 +3,17 @@ package main
 import (
 
 	"github.com/wix-private/BazelBuildForGo/project1/p1lib"
+	"github.com/coreos/etcd/client"
 )
 
+type EtcdClientAPI struct {
+	client  client.KeysAPI
+}
+
 func main(){
+	_ = &EtcdClientAPI{
+		client : nil,
+	}
 	println ("Project1 called")
 	res := p1lib.DoSomeWorksP1()
 	println (res)
